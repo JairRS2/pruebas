158d03608ceebd1844459679230cea8a934b2f93
Jair <urielrosas4321@gmail.com>
2025-01-17 13:30:48 -0600

    yayayaya

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const cors = require('cors'); // Importa el paquete cors
 const productsRoutes = require('./routes/products.routes');
 
 const app = express();
+
+// Habilita CORS para todas las peticiones (cualquier origen)
+app.use(cors());
+
+// Middleware para parsear el cuerpo de las solicitudes en formato JSON
 app.use(express.json());
-//
+
 // Rutas
 app.use('/api/products', productsRoutes);
 
